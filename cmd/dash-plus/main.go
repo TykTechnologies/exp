fa8a37791ea588214b8235b5dff0bbf68eb1238f
Tit Petric <tit@tyk.io>
2023-05-19 12:56:02 +0200

    Add code comment

--- cmd/dash-plus/main.go
+++ cmd/dash-plus/main.go
@@ -5,6 +5,18 @@ import (
 	"github.com/TykTechnologies/tyk/gateway"
 )
 
+// Dashboard and Gateway, together
+//
+// This program imports dashboard and gateway, running the services on their
+// individual ports. However, if dashboard would define a `http.Handler`, we
+// would not need a secondary HTTP server for the dashboard, but could
+// register that handler into the Gateway.
+//
+// The idea is, that Gateway has all this functionality which can be used in
+// front of the Dashboard as well. The dashboard provides an openapi spec,
+// and could itself have an api definition for the gateway to use
+// implemented functionality.
+
 func main() {
 	var shutdownOnce sync.Once
 
