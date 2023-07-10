cf8aef8af0017ac40100e1cdfb8a73e7a27adb8e
Tit Petric <tit@tyk.io>
2023-07-10 17:18:52 +0200

    Add placeholder for schema-gen restore --fix

--- cmd/schema-gen/restore/options.go
+++ cmd/schema-gen/restore/options.go
@@ -7,6 +7,8 @@ import (
 )
 
 type options struct {
+	fix bool
+
 	inputFile   string
 	outputFile  string
 	packageName string
@@ -25,8 +27,10 @@ func NewOptions() *options {
 	flag.StringVarP(&cfg.outputFile, "output-file", "o", cfg.outputFile, "output file")
 	flag.StringVarP(&cfg.inputFile, "input-file", "i", cfg.inputFile, "input file")
 	flag.StringVarP(&cfg.packageName, "package-name", "p", cfg.packageName, "package name")
-	flag.StringSliceVarP(&cfg.keep, "keep", "", cfg.keep, "type definition names to keep (default: all)")
-	flag.StringSliceVarP(&cfg.includeFunctions, "include-functions", "", cfg.includeFunctions, "include functions (default: none)")
+
+	flag.StringSliceVar(&cfg.keep, "keep", cfg.keep, "type definition names to keep (default: all)")
+	flag.StringSliceVar(&cfg.includeFunctions, "include-functions", cfg.includeFunctions, "include functions (default: none)")
+	flag.BoolVar(&cfg.fix, "fix", cfg.fix, "generate output files according to struct names")
 	flag.StringVar(&cfg.rootElement, "root", cfg.rootElement, "root type to put first in output")
 	flag.Parse()
 
