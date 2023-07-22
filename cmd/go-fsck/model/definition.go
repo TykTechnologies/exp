174a61845a33652b87a3c8d304eccf90a022d2f1
Tit Petric <tit@tyk.io>
2023-07-22 12:30:01 +0200

    Separate fill/order

--- cmd/go-fsck/model/definition.go
+++ cmd/go-fsck/model/definition.go
@@ -54,13 +54,19 @@ func (d *Definition) getImports(decl *Declaration) []string {
 	return d.Imports.Get(decl.File)
 }
 
-func (d *Definition) Fill() {
-	all := append([]*Declaration{}, d.Types...)
-	all = append(all, d.Funcs...)
-	all = append(all, d.Vars...)
-	all = append(all, d.Consts...)
+func (d *Definition) Order() []*Declaration {
+	count := len(d.Types) + len(d.Funcs) + len(d.Vars) + len(d.Consts)
+	result := make([]*Declaration, 0, count)
 
-	for _, decl := range all {
+	result = append(result, d.Types...)
+	result = append(result, d.Funcs...)
+	result = append(result, d.Vars...)
+	result = append(result, d.Consts...)
+	return result
+}
+
+func (d *Definition) Fill() {
+	for _, decl := range d.Order() {
 		decl.Imports = d.getImports(decl)
 	}
 }
