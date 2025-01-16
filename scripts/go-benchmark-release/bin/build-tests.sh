396d40f009a27c5f464babd8b8d7cbe51cb81014
Tit Petric <tit@tyk.io>
2025-01-16 17:14:22 +0100

    Update build

--- scripts/go-benchmark-release/bin/build-tests.sh
+++ scripts/go-benchmark-release/bin/build-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-releases="v5.0.15 v5.1.2 v5.2.6 v5.3.9"
+source .env.sh
 
 # This builds test binaries for releases since v5.0-5.3, on latest patch releases.
 # The binaries are *NOT* statically compiled so runtime environment is an issue.
@@ -8,16 +8,17 @@ mkdir -p tests
 testFolder=$PWD/tests
 
 # Build test binaries
-for release in $releases; do
+for release in "${!releases[@]}"; do
 	mkdir -p cache/$release-build cache/pkg
 
 	out=tyk-$release.test
+	branch="${releases[$release]}"
 	src=$PWD/src/tyk-$release
 	srcInside=/go/pkg/github.com/TykTechnologies/tyk
 
 	# Check out tyk if required
 	if [ ! -d "$src" ]; then
-		git clone --depth 1 --branch $release git@github.com:TykTechnologies/tyk.git $src
+		git clone --depth 1 --branch $branch git@github.com:TykTechnologies/tyk.git $src
 	fi
 
 	# Build tests for release
@@ -27,6 +28,7 @@ for release in $releases; do
 		# Use plugin compiler as build env
 		docker run --rm -v $src:$srcInside -w $srcInside -v $testFolder:/tests \
 			-e GO111MODULE=on \
+			-e GOTOOLCHAIN=auto \
 			-v $PWD/cache/pkg:/go/pkg \
 			-v $PWD/cache/$release-build:/root/.cache/go-build \
 			--entrypoint=/bin/bash tykio/tyk-plugin-compiler:$release -c "env | grep ^GO ; go mod download ; go test -o /tests/$out -trimpath -tags 'goplugin dev' -c ./gateway"
@@ -41,14 +43,14 @@ done
 # the go toolchain base image used somehow (dev dockerfile, gha?).
 
 release=master
-
+branch=master
 mkdir -p cache/$release-build cache/pkg
 
 out=tyk-$release.test
 src=$PWD/src/tyk-$release
 
 if [ ! -d "$src" ]; then
-	git clone --branch 'fix/tt-13819/benchmark-run-updates' git@github.com:TykTechnologies/tyk.git $src
+	git clone --branch $branch git@github.com:TykTechnologies/tyk.git $src
 fi
 
 if [ ! -f "$testFolder/$out" ]; then
